ee6a4d7a06d5eb7451aec4d6d9cb75c1fa42547e
alemfla3181 <alemfla3181@naver.com>
2021-11-19 14:00:09 +0900

    movie_app

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '3f89248c0e8425578afdf04bf9a4f0e2';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
